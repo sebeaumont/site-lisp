a60e94ab557d683ad92a31e5db108f19049e9500
Simon Beaumont <datalligator@icloud.com>
2022-11-30 15:21:27 +0000

    renamed to org-present-extra

--- org-present-extra.el
+++ org-present-extra.el
@@ -1,4 +1,4 @@
-;;; org-present-xtra --- Some extras to make org mode nicer to look at
+;;; org-present-extra --- Some extras to make org mode nicer to look at
 ;;; Commentary:
 ;;;  Lots of font and variable hacking that can be toggled
 
@@ -9,21 +9,30 @@
 (require 'org)
 (require 'org-faces)
 
-(defun xtra/org-present-start ()
+(defcustom org-present-extra-default
+  '(default :height 1.5)
+  "Default."
+  :group 'org-present-extra)
+
+;;;###autoload
+(defun org-present-extra-start ()
   "Enter org presentation mode."
   (interactive)
   (set-variable 'org-hide-emphasis-markers t)
   (xtra/hide-org-block-markers)
-  ;; (xtra/resize-org-faces)
+  (xtra/resize-org-faces)
   (org-mode-restart))
 
-(defun xtra/org-present-end ()
+;;;###autoload
+(defun org-present-extra-end ()
   "Leave org presentation mode."
   (interactive)
   (set-variable 'org-hide-emphasis-markers nil)
   (xtra/show-org-block-markers)
-  ;;(xtra/reset-org-faces)
+  (xtra/reset-org-faces)
   (org-mode-restart))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun xtra/hide-org-block-markers ()
   "Hide org mode block markers."
@@ -45,27 +54,37 @@
   (set-face-attribute
    'org-block-end-line nil :foreground nil :background nil))
 
-;;; the following don't seem to work maybe some interaction
-;;; with the org-slide?
+;;; turn and face the change...
+
+
+(defvar-local xtra-default-cookie nil)
+(defvar-local xtra-header-cookie nil)
+(defvar-local xtra-org-document-title-cookie nil)
+(defvar-local xtra-org-code-cookie nil)
+(defvar-local xtra-org-verbatim-cookie nil)
+(defvar-local xtra-org-block-cookie nil)
+(defvar-local xtra-org-block-begin-line-cookie nil)
 
 (defun xtra/resize-org-faces ()
   "Tweak org mode faces - YMMV."
   (setq-local
-   face-remapping-alist
-   '((default (:height 1.5) default)
-     (header-line (:height 5.0) header-line)
-     (org-document-title (:height 5.0) org-document-title)
-     (org-code (:height 1.5) org-code)
-     (org-verbatim (:height 1.5) org-verbatim)
-     (org-block (:height 1.25) org-block)
-     (org-block-begin-line (:height 0.5) org-block-begin-line))))
+   xtra-default-cookie
+   (face-remap-add-relative 'default org-present-extra-default)))
 
 (defun xtra/reset-org-faces ()
   "Reset org mode faces."
-  (setq-local
-   face-remapping-alist
-   '((default varaible-pitch default))))
-                
+  (face-remap-remove-relative xtra-default-cookie))
 
-(provide 'org-present-xtra)
-;;; org-present-xtra.el ends here
+
+
+;   '((default (:height 1.5) default)
+;     (header-line (:height 5.0) header-line)
+;     (org-document-title (:height 5.0) org-document-title)
+;     (org-code (:height 1.5) org-code)
+;     (org-verbatim (:height 1.5) org-verbatim)
+;     (org-block (:height 1.25) org-block)
+;     (org-block-begin-line (:height 0.5) org-block-begin-line))))
+
+
+(provide 'org-present-extra)
+;;; org-present-extra.el ends here
